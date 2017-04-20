26e85b1e5c8ed52e8da106599b082552c0ace3b0
Jae Huh <jaeeunhuh@gmail.com>
2017-04-20 15:19:18 +1200

    Refine apple picking function

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var apple = document.getElementsByClassName('apple')
+var appleDiv = document.getElementById('apple-container')
+
 var fruitCounter = document.getElementById('fruit-count')
 var fruitCount = 0;
+var water = document.getElementById('watering-can')
 
 
-for (var i = 0; i < apple.length; i++) {
-  apple[i].addEventListener('click', function(event) {
-    // apple[i].parentNode.removeChild(apple[i])
-    event.target.parentNode.removeChild(event.target)
-    fruitCount += 1
-    fruitCounter.innerHTML = fruitCount
-  })
+// water.addEventListener
+
+// click apples to pick and show the number of picked apples on the basket
+appleDiv.addEventListener('click', function(event) {
+  pickApple(event.target)
+})
+
+function pickApple(el) {
+  el.parentNode.removeChild(el)
+  fruitCount += 1
+  fruitCounter.innerHTML = fruitCount
 }
