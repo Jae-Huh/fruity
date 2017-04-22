99b2b7be4c02a79915c7808cb7bc4239d586cec8
Jae Huh <jaeeunhuh@gmail.com>
2017-04-23 00:49:15 +1200

    Resets game when game is over

--- script.js
+++ script.js
@@ -8,9 +8,40 @@ var rottenAppleCount = 0
 
 // Watering - makes apples grow but when watered too much, they rot
 water.addEventListener('click', function() {
-    var randomNumber = Math.floor(Math.random() * 5)
-    growApples(randomNumber)
+
+  for (var i = 0; i < apple.length; i++) {
+     if (apple[i].src !== 'file:///Users/JaeH/devacademy/fruity/images/apple.png') {
+       rottenAppleCount += 1
+     } else {
+       rottenAppleCount = 0
+     }
+   }
+
+   if (rottenAppleCount <= 5) {
+     var randomNumber = Math.floor(Math.random() * 5)
+     growApples(randomNumber)
+
+   } else {
+     var confirmRestart = confirm('Game Over! Click "OK" to start the game again.')
+     if (confirmRestart) {
+       resetGame()
+     }
+   }
+
+    // var randomNumber = Math.floor(Math.random() * 5)
+    // growApples(randomNumber)
 })
+
+function resetGame() {
+  for (var i = 0; i < apple.length; i++) {
+    apple[i].setAttribute('src', 'images/apple.png')
+    apple[i].style.height = '0vh'
+    apple[i].style.display = 'initial'
+  }
+  score = 0
+  scoreNum.innerHTML = score
+  rottenAppleCount = 0
+}
 
 function growApples(randomNumber) {
   if (!apple[randomNumber] || apple[randomNumber].getAttribute('src') === 'images/apple-rotten.png') {
@@ -33,7 +64,8 @@ appleDiv.addEventListener('click', function(event) {
 })
 
 function pickApple(el) {
-  el.parentNode.removeChild(el)
+  // el.parentNode.removeChild(el)
+  el.style.display = 'none'
   switch(el.style.height) {
     case '3vh':
       score += 1
