9ea755fac9b3ebd1f777379c5bba447e94da42ad
Jae Huh <jaeeunhuh@gmail.com>
2017-04-22 16:25:27 +1200

    Improve growApples() function by adding size check and invoking self when already picked apple is chosen

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 var apple = document.getElementsByClassName('apple')
 var appleDiv = document.getElementById('apple-container')
-
 var scoreNum = document.getElementById('scoreNum')
 var score = 0
-
 var water = document.getElementById('watering-can')
-var apple1height = 0
-var apple2height = 0
-var apple3height = 0
-var apple4height = 0
-var apple5height = 0
+
 
 // Watering - makes apples grow but when watered too much, they rot
 water.addEventListener('click', function() {
@@ -17,66 +11,20 @@ water.addEventListener('click', function() {
   growApples(randomNumber)
 })
 
-  // This could be refactored to reduce code.
-  // You could create a function that accepts the random number
-  // And applys the going / if statement to that number
-  // e.g.
-  // apple[randNum].setAttribute('src', 'images/apple-rotten.png')
-  // You also already have the hieght as
-  // apple[0].style.height, so instead fo storing it as a var you could just check that value
-
-  function growApples(randomNumber) {
-    if (apple[randomNumber].style.height === '18vh') {
-      apple[randomNumber].setAttribute('src', 'images/apple-rotten.png')
-    } else {
-      apple[randomNumber].style.height = parseInt(apple[randomNumber].style.height) + 3 + 'vh'
-    }
+function growApples(randomNumber) {
+  if (!apple[randomNumber]) {
+    return growApples(Math.floor(Math.random() * 5))
   }
+  if (apple[randomNumber].style.height === '18vh') {
+    apple[randomNumber].setAttribute('src', 'images/apple-rotten.png')
+  } else {
+    if (!apple[randomNumber].style.height) {
+      apple[randomNumber].style.height = '3vh'
+    }
+    apple[randomNumber].style.height = parseInt(apple[randomNumber].style.height) + 3 + 'vh'
+  }
+}
 
-
-//   if (randomNumber === 0) {
-//     if (apple1height === 18) {
-//       apple[0].style.height = '18vh'
-//       apple[0].setAttribute('src', 'images/apple-rotten.png')
-//     } else {
-//       apple1height += 3
-//       apple[0].style.height = apple1height + 'vh'
-//     }
-//   } else if (randomNumber === 1) {
-//     if (apple2height === 18) {
-//       apple[1].style.height = '18vh'
-//       apple[1].setAttribute('src', 'images/apple-rotten.png')
-//     } else {
-//       apple2height += 3
-//       apple[1].style.height = apple2height + 'vh'
-//     }
-//   } else if (randomNumber === 2) {
-//     if (apple3height === 18) {
-//       apple[2].style.height = '18vh'
-//       apple[2].setAttribute('src', 'images/apple-rotten.png')
-//     } else {
-//       apple3height += 3
-//       apple[2].style.height = apple3height + 'vh'
-//     }
-//   } else if (randomNumber === 3) {
-//     if (apple4height === 18) {
-//       apple[3].style.height = '18vh'
-//       apple[3].setAttribute('src', 'images/apple-rotten.png')
-//     } else {
-//       apple4height += 3
-//       apple[3].style.height = apple4height + 'vh'
-//     }
-//   } else if (randomNumber === 4) {
-//     if (apple5height === 18) {
-//       apple[4].style.height = '18vh'
-//       apple[4].setAttribute('src', 'images/apple-rotten.png')
-//     } else {
-//       apple5height += 3
-//       apple[4].style.height = apple5height + 'vh'
-//     }
-//   }
-//   console.log(randomNumber, apple1height, apple2height, apple3height, apple4height)
-// })
 
 // Click apples to pick and show the number of picked apples on the basket
 appleDiv.addEventListener('click', function(event) {
